0404b9a02ee52a56cb1c1d745267e14656dbb17c
Sambapaulo <topetchic@gmail.com>
2026-05-19 15:30:16 +0200

    fix: force unregister SW and clear all caches

--- public/sw.js
+++ public/sw.js
@@ -1,41 +1,14 @@
-const CACHE_NAME = 'ghostmeter-v7';
-
-self.addEventListener('install', (event) => {
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('activate', () => {
+  caches.keys().then((names) => {
+    names.forEach((name) => caches.delete(name));
+  });
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET' || event.request.url.includes('/api/')) {
-    return;
-  }
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        if (response && response.status === 200 && response.type === 'basic') {
-          const responseToCache = response.clone();
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, responseToCache);
-          });
-        }
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
+  event.respondWith(fetch(event.request));
 });
