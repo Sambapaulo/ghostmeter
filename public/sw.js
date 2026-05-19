a5f30ac8483664c94ceb6f408578fc225d30964f
Sambapaulo <topetchic@gmail.com>
2026-05-19 15:36:47 +0200

    fix: SW self-unregister to force cache clear

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,1 @@
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', () => {
-  caches.keys().then((names) => {
-    names.forEach((name) => caches.delete(name));
-  });
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener("install", () => { self.registration.unregister(); });
